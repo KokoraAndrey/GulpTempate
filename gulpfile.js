9c41ef0230c02a752e103289abd6e7148b6e0ac3
Andrey Kokora <andrey18121996@gmail.com>
2023-05-15 21:33:09 +0300

    improvement template

--- gulpfile.js
+++ gulpfile.js
@@ -17,6 +17,13 @@ const del = require('del');
 const notify = require('gulp-notify');
 const imagewebp = require('gulp-webp');
 const browserSync = require('browser-sync').create();
+const babel = require('gulp-babel');
+const svgSprite = require('gulp-svg-sprite');
+const svgMin = require('gulp-svgmin');
+const concat = require('gulp-concat');
+const fonter = require('gulp-fonter');
+const ttf2woff2 = require('gulp-ttf2woff2');
+const stylelint = require('gulp-stylelint');
 
 /* Paths */
 const srcPath = 'src/';
@@ -27,25 +34,32 @@ const path = {
         html: distPath,
         css: distPath + 'assets/css/',
         js: distPath + 'assets/js/',
-        images: distPath + 'assets/img/',
+        vendors: distPath + 'assets/js/vendors/',
+        images: distPath + 'assets/images/',
+        sprite: distPath + 'assets/images/sprite',
         fonts: distPath + 'assets/fonts/',
     },
+
     src: {
         html: srcPath + '*.html',
         css: srcPath + 'assets/scss/*.scss',
         js: srcPath + 'assets/js/*.js',
         images:
             srcPath +
-            'assets/img/**/*.{jpg,png,svg,gif,ico,webp,webmanifest,xml,json}',
+            'assets/images/**/*.{jpg,png,svg,gif,ico,webp,webmanifest,xml,json}',
+        sprite: srcPath + 'assets/images/sprite/*.svg',
         fonts: srcPath + 'assets/fonts/**/*.{eot,woff,woff2,ttf,svg}',
+        scss: './src/assets/scss/**/*.scss',
     },
+
     watch: {
         html: srcPath + '**/*.html',
         js: srcPath + 'assets/js/**/*.js',
         css: srcPath + 'assets/scss/**/*.scss',
         images:
             srcPath +
-            'assets/img/**/*.{jpg,png,svg,gif,ico,webp,webmanifest,xml,json}',
+            'assets/images/**/*.{jpg,png,svg,gif,ico,webp,webmanifest,xml,json}',
+        sprite: srcPath + 'assets/images/sprite/*.svg',
         fonts: srcPath + 'assets/fonts/**/*.{eot,woff,woff2,ttf,svg}',
     },
     clean: './' + distPath,
@@ -111,6 +125,22 @@ function css() {
         .pipe(browserSync.reload({ stream: true }));
 }
 
+//* Stylelint
+function testScssLint() {
+    return src(path.src.scss).pipe(
+        stylelint({
+            reporters: [
+                {
+                    failAfterError: true,
+                    formatter: 'string',
+                    console: true,
+                },
+            ],
+        })
+    );
+}
+
+//* JavaScript
 function js() {
     return src(path.src.js, { base: srcPath + 'assets/js/' })
         .pipe(
@@ -122,6 +152,11 @@ function js() {
                     })(err);
                     this.emit('end');
                 },
+            })
+        )
+        .pipe(
+            babel({
+                presets: ['@babel/env'],
             })
         )
         .pipe(rigger())
@@ -138,7 +173,11 @@ function js() {
 }
 
 function images() {
-    return src(path.src.images, { base: srcPath + 'assets/img/' })
+    return src(
+        path.src.images,
+        { base: srcPath + 'assets/images/' },
+        !path.src.sprite
+    )
         .pipe(
             imagemin([
                 imagemin.gifsicle({ interlaced: true }),
@@ -153,14 +192,36 @@ function images() {
         .pipe(browserSync.reload({ stream: true }));
 }
 
+function sprite() {
+    return src(path.src.sprite, { base: srcPath + 'assets/images/sprite/' })
+        .pipe(
+            svgSprite({
+                mode: {
+                    stack: {
+                        sprite: 'sprite.svg',
+                        example: true,
+                    },
+                },
+            })
+        )
+        .pipe(dest(path.build.sprite));
+}
+
 function webpImages() {
-    return src(path.src.images, { base: srcPath + 'assets/img/' })
+    return src(path.src.images, { base: srcPath + 'assets/images/' })
         .pipe(imagewebp())
         .pipe(dest(path.build.images));
 }
 
 function fonts() {
     return src(path.src.fonts, { base: srcPath + 'assets/fonts/' })
+        .pipe(
+            fonter({
+                formats: ['woff', 'ttf'],
+            })
+        )
+        .pipe(src(srcPath + 'assets/fonts/*.ttf'))
+        .pipe(ttf2woff2())
         .pipe(dest(path.build.fonts))
         .pipe(browserSync.reload({ stream: true }));
 }
@@ -174,19 +235,32 @@ function watchFiles() {
     gulp.watch([path.watch.css], css);
     gulp.watch([path.watch.js], js);
     gulp.watch([path.watch.images], images);
+    gulp.watch([path.watch.sprite], sprite);
     gulp.watch([path.watch.fonts], fonts);
 }
 
 const build = gulp.series(
     clean,
-    gulp.parallel(html, css, js, images, webpImages, fonts)
+
+    //* Если есть сторонние библиотеки, добавить vendors
+    gulp.parallel(html, css, js, images, sprite, webpImages, fonts)
 );
 const watch = gulp.parallel(build, watchFiles, serve);
 
+//* Если есть сторонние библиотеки которые требуется подключить, нужно раскомментировать функцию vendor() и в src указать путь к файлу
+// function vendors() {
+//     return src(['node_modules/slick-carousel/slick/slick.min.js'])
+//         .pipe(concat('libs.js'))
+//         .pipe(dest(path.build.vendors));
+// }
+
 exports.html = html;
 exports.css = css;
+exports.lint = testScssLint;
 exports.js = js;
+// exports.vendors = vendors;
 exports.images = images;
+exports.sprite = sprite;
 exports.webpImages = webpImages;
 exports.fonts = fonts;
 exports.clean = clean;
